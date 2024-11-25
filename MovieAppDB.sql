43054c3b26765f0953e23eda19b13381b11392b4
tem87 <143668841+tem87@users.noreply.github.com>
2024-11-24 22:08:30 -0700

    Update MovieAppDB.sql

--- MovieAppDB.sql
+++ MovieAppDB.sql
@@ -71,7 +71,7 @@ CREATE TABLE Bank (
 );
 
 CREATE TABLE Voucher (
-    voucher_id INT AUTO_INCREMENT PRIMARY KEY,
+    voucher_id INT NOT NULL PRIMARY KEY,
     user_id INT NOT NULL,
     amount DECIMAL(8, 2) NOT NULL, -- Value of the voucher
     is_used BOOLEAN DEFAULT FALSE, -- Indicates if the voucher has been redeemed
@@ -100,14 +100,15 @@ INSERT INTO Theater (name, location) VALUES
 
 -- Insert showtimes
 INSERT INTO Showtime (movie_id, theater_id, start_time, max_seats) VALUES
-(1, 1, '2024-11-20 17:00', 10),
-(1, 2, '2024-11-20 18:00', 5),
+(1, 1, '2024-11-25 17:00', 10), -- less than 72 hours
+(1, 2, '2024-11-30 18:00', 5), -- more
 (2, 1, '2024-11-21 19:00', 10),
 (2, 2, '2024-11-21 20:00', 10),
 (3, 1, '2024-11-22 22:00', 10),
 (3, 2, '2024-11-21 20:00', 10),
 (4, 1, '2024-11-22 22:00', 10),
 (4, 2, '2024-11-21 20:00', 10);
+
 
 -- Generate seats for showtime_id = 1 (10 seats)
 INSERT INTO Seats (showtime_id, seat_number) VALUES
@@ -121,10 +122,38 @@ INSERT INTO Seats (showtime_id, seat_number) VALUES
 -- Generate seats for showtime_id = 3 (5 seats)
 INSERT INTO Seats (showtime_id, seat_number) VALUES
 (3, 'B1'), (3, 'B2'), (3, 'B3'), (3, 'B4'), (3, 'B5');
+-- c and d are for me before you
+-- Generate seats for showtime_id = 7 (10 seats as per max_seats)
+INSERT INTO Seats (showtime_id, seat_number) VALUES
+(7, 'C1'), (7, 'C2'), (7, 'C3'), (7, 'C4'), (7, 'C5'),
+(7, 'C6'), (7, 'C7'), (7, 'C8'), (7, 'C9'), (7, 'C10');
+
+-- Generate seats for showtime_id = 8 (10 seats as per max_seats)
+INSERT INTO Seats (showtime_id, seat_number) VALUES
+(8, 'D1'), (8, 'D2'), (8, 'D3'), (8, 'D4'), (8, 'D5'),
+(8, 'D6'), (8, 'D7'), (8, 'D8'), (8, 'D9'), (8, 'D10');
+
+-- E and F are for spirited away
+-- Generate seats for showtime_id = 5 (10 seats)
+INSERT INTO Seats (showtime_id, seat_number) VALUES
+(5, 'E1'), (5, 'E2'), (5, 'E3'), (5, 'E4'), (5, 'E5'),
+(5, 'E6'), (5, 'E7'), (5, 'E8'), (5, 'E9'), (5, 'E10');
+
+-- Generate seats for showtime_id = 6 (10 seats)
+INSERT INTO Seats (showtime_id, seat_number) VALUES
+(6, 'F1'), (6, 'F2'), (6, 'F3'), (6, 'F4'), (6, 'F5'),
+(6, 'F6'), (6, 'F7'), (6, 'F8'), (6, 'F9'), (6, 'F10');
+-- h is for shrek 2 for theater id 2
+-- Generate seats for showtime_id = 4 (10 seats)
+INSERT INTO Seats (showtime_id, seat_number) VALUES
+(4, 'H1'), (4, 'H2'), (4, 'H3'), (4, 'H4'), (4, 'H5'),
+(4, 'H6'), (4, 'H7'), (4, 'H8'), (4, 'H9'), (4, 'H10');
+
+
 
 INSERT INTO Users (name, email, password, phone_number, address, is_registered, annual_fee_paid) VALUES
 ('Jamie Smith', 'jamie1@gmaile.com', NULL, NULL, NULL, FALSE, FALSE),
-('Tony Sanchez', 'tony2@example.com', 'password456&', '0987654321', '596 Oak Avenue', FALSE, FALSE);
+('a', 'a@gmail.com', 'a', '0987654321', '596 Oak Avenue', TRUE, FALSE);
 
 
 
